7769a0df13a20221bc836b77a6d46cb52d7a8904
Matheus Gomes <matheusgprlima@gmail.com>
2021-02-04 20:08:57 -0300

    Creating Search Movies

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ListMovies } from "./pages/ListMovies";
+import { SearchMovies } from "./pages/SearchMovies";
 export function Routers() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={ListMovies} />
+        <Route path="/" exact component={SearchMovies} />
+        <Route path="/list-movies" component={ListMovies} />
       </Switch>
     </BrowserRouter>
   );
