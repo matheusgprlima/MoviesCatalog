be5893b5864d8c3a9c585f6329f5648576dfbbbe
Matheus Gomes <matheusgprlima@gmail.com>
2021-02-16 23:15:59 -0300

    Fixing deploy

--- src/services/omdbApi.ts
+++ src/services/omdbApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const omdbApi = axios.create({
-  baseURL: "http://www.omdbapi.com/",
+  baseURL: "https://www.omdbapi.com/",
   params: { apikey: process.env.REACT_APP_API_KEY },
 });
 
